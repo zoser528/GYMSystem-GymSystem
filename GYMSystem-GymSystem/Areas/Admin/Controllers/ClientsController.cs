ce684c648a9190ce78d8ee9d36bc89d31dc70988
Ahmed Hassan <93221571+zoser528@users.noreply.github.com>
2023-09-06 06:35:22 -0700

    V3.1
    handle some bugs

--- GYMSystem-GymSystem/Areas/Admin/Controllers/ClientsController.cs
+++ GYMSystem-GymSystem/Areas/Admin/Controllers/ClientsController.cs
@@ -12,6 +12,8 @@ using GYMSystem.Models;
 using Humanizer;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using Microsoft.VisualBasic;
+using System.Xml.Linq;
+using OfficeOpenXml;
 
 namespace GYMSystem_GymSystem.Areas.Admin.Controllers
 {
@@ -112,6 +114,7 @@ namespace GYMSystem_GymSystem.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+
         public async Task<IActionResult> Create([Bind("ClientId,ClientName,UserName,UserPassword,ClientNumber,DOB,SubscriptionDate,ClientAddress,ClientEmail,Branchid,Departmentid,Trainerid,Subscriptionid")] Client client)
         {
             
@@ -124,10 +127,21 @@ namespace GYMSystem_GymSystem.Areas.Admin.Controllers
             ModelState.Remove("TrainerName");
             ModelState.Remove("TrainerId");
             var subscription = await _context.Subscriptions.FindAsync(client.Subscriptionid);
+            var trainer = await _context.Trainers.FindAsync(client.Trainerid);
+
             
             if (ModelState.IsValid)
             {
+                if(trainer.Counter != null)
+                {
+                    trainer.Counter++;
+                }
+                else
+                {
+                    trainer.Counter = 0;
+                }
                 client.End_Subscription = (DateTime.Now.AddMonths(subscription.SubscriptionPeriod));
+                
                 _context.Add(client);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -148,11 +162,15 @@ namespace GYMSystem_GymSystem.Areas.Admin.Controllers
             {
                 return NotFound();
             }
-
             var client = await _context.Clients.FindAsync(id);
+            var trainer = await _context.Trainers.FindAsync(client.Trainerid);
             if (client == null)
             {
                 return NotFound();
+            }
+            if (trainer != null)
+            {
+                trainer.Counter = trainer.Counter - 1;
             }
             ViewData["Branchid"] = new SelectList(_context.Branches, "BranchId", "BranchLocation", client.Branchid);
             ViewData["Departmentid"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentCode", client.Departmentid);
@@ -168,15 +186,26 @@ namespace GYMSystem_GymSystem.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ClientId,ClientName,UserName,UserPassword,ClientNumber,DOB,SubscriptionDate,ClientAddress,ClientEmail,Branchid,Departmentid,Trainerid,Subscriptionid")] Client client)
         {
+            var trainer = await _context.Trainers.FindAsync(client.Trainerid);
+
             if (id != client.ClientId)
             {
                 return NotFound();
             }
 
+            ModelState.Remove("Branch");
+            ModelState.Remove("BranchName");
+            ModelState.Remove("Department");
+            ModelState.Remove("DepartmentName");
+            ModelState.Remove("SubscriptionName");
+            ModelState.Remove("Subscription");
+            ModelState.Remove("TrainerName");
+            ModelState.Remove("TrainerId");
             if (ModelState.IsValid)
             {
                 try
                 {
+                    trainer.Counter += 1;
                     _context.Update(client);
                     await _context.SaveChangesAsync();
                 }
@@ -232,8 +261,14 @@ namespace GYMSystem_GymSystem.Areas.Admin.Controllers
                 return Problem("Entity set 'ApplicationDbContext.Clients'  is null.");
             }
             var client = await _context.Clients.FindAsync(id);
+            var trainer = await _context.Trainers.FindAsync(client.Trainerid);
+
             if (client != null)
             {
+                if(trainer.Counter != null)
+                {
+                    trainer.Counter = trainer.Counter - 1;
+                }   
                 _context.Clients.Remove(client);
             }
             
@@ -245,6 +280,60 @@ namespace GYMSystem_GymSystem.Areas.Admin.Controllers
         {
           return (_context.Clients?.Any(e => e.ClientId == id)).GetValueOrDefault();
         }
+        
 
+        public IActionResult ExportToExcel()
+        {
+            var data = GetClients();
+
+
+            // Set the EPPlus LicenseContext
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // For non-commercial use
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Data");
+
+                // Set the header row
+                worksheet.Cells[1, 1].Value = "Client Name";
+                worksheet.Cells[1, 2].Value = "Client Number";
+                worksheet.Cells[1, 3].Value = "date of birth";
+                worksheet.Cells[1, 4].Value = "Time to End Subscription";
+                worksheet.Cells[1, 5].Value = "Client Address";
+                worksheet.Cells[1, 6].Value = "Department Name";
+                worksheet.Cells[1, 7].Value = "Branch Name";
+                worksheet.Cells[1, 8].Value = "Subscription Name";
+                worksheet.Cells[1, 9].Value = "Trainer Name";
+                worksheet.Cells[1, 10].Value = "is Active!";
+                worksheet.Cells[1, 11].Value = "paid";
+                // Add more headers for each column as needed
+
+                // Fill data rows
+                for (int i = 0; i < data.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = data[i].ClientName;
+                    worksheet.Cells[i + 2, 2].Value = data[i].ClientNumber;
+                    worksheet.Cells[i + 2, 3].Value = data[i].DOB;
+                    worksheet.Cells[i + 2, 4].Value = data[i].End_Subscription;
+                    worksheet.Cells[i + 2, 5].Value = data[i].ClientAddress;
+                    worksheet.Cells[i + 2, 6].Value = data[i].DepartmentName;
+                    worksheet.Cells[i + 2, 7].Value = data[i].BranchName;
+                    worksheet.Cells[i + 2, 8].Value = data[i].SubscriptionName;
+                    worksheet.Cells[i + 2, 9].Value = data[i].TrainerName;
+                    worksheet.Cells[i + 2, 10].Value = data[i].Active;
+                    worksheet.Cells[i + 2, 11].Value = data[i].pay;
+                    // Add more data columns as needed
+                }
+
+                // Set content type and file name
+                Response.Headers.Add("Content-Disposition", "attachment; filename=ExportedData.xlsx");
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+                // Write the Excel package to the response stream
+                Response.Body.Write(package.GetAsByteArray());
+            }
+
+            return new EmptyResult();
+        }
     }
 }
